cb39bef38fa6e39eb6ee935a09c60dcceea1cc81
Bishor <84746849+Peteribi73@users.noreply.github.com>
2023-10-03 03:14:50 -0300

    algo de estilo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,68 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
+//agregar algo que te diga lo fuerte que es la contraseña (basandose solo en la longitud de la contraseña).
+
 function App() {
-  const [count, setCount] = useState(0);
+  const [range, setRange] = useState(15);
+  const [numberRange, setNumberRange] = useState(0);
+  const [score, setScore] = useState("weak");
+
+  const handleRangeOnChange = (e) => {
+    setRange(e.target.value);
+    setNumberRange(e.target.value);
+  };
 
   return (
-    <>
-      <main className="app">
-        <h1>Password Generator</h1>
-        <p>Generate a random password</p>
-        <section className="inputs">
-          <form action="">
-            <label htmlFor="length">Length: </label>
-            <input type="number" name="length" id="length" min="1" max="100" />
-            <label htmlFor="uppercase">Uppercase: </label>
-            <input type="checkbox" name="uppercase" id="uppercase" />
-            <label htmlFor="lowercase">Lowercase: </label>
-            <input type="checkbox" name="lowercase" id="lowercase" />
-            <label htmlFor="numbers">Numbers: </label>
-            <input type="checkbox" name="numbers" id="numbers" />
-            <label htmlFor="symbols">Symbols: </label>
-            <input type="checkbox" name="symbols" id="symbols" />
-            <label htmlFor="security">Security level*: </label>
-            <input type="range" name="security" id="security" min="1" max="3" />
-            // copilot show security level with a number above the range input
-          </form>
-        </section>
-      </main>
-    </>
+    <main className="app">
+      <h1>Password Generator</h1>
+      {/* <p>--Generate a random password--</p> */}
+      <p>Your password's score: {score}</p>
+      <div className="password"></div>
+
+      <section className="show">
+        {/* <h2>Generated Password</h2> */}
+        {/* Your password will appear here */}
+        <p id="password">aKANJ789ASMD0-,DFI8</p>
+        <div className="functionalityButtons">
+          <button id="copyBtn">
+            <i className="far fa-clipboard"></i> Copy to Clipboard
+          </button>
+          <button id="regenerateBtn">
+            <i className="fa-solid fa-rotate"></i> Regenerate
+          </button>
+        </div>
+      </section>
+
+      <section className="inputs">
+        <form action="">
+          <input type="checkbox" name="uppercase" id="uppercase" />
+          <label htmlFor="uppercase">A-Z </label>
+
+          <input type="checkbox" name="lowercase" id="lowercase" />
+          <label htmlFor="lowercase">a-z </label>
+
+          <input type="checkbox" name="numbers" id="numbers" />
+          <label htmlFor="numbers">0-9 </label>
+
+          <input type="checkbox" name="symbols" id="symbols" />
+          <label htmlFor="symbols">!@#$%^&* </label>
+
+          <p id="rangeValue">Characters: {range}</p>
+
+          <label htmlFor="length"></label>
+          <input
+            type="range"
+            name="length"
+            id="length"
+            min="8"
+            max="50"
+            value={range}
+            onChange={handleRangeOnChange}
+          />
+        </form>
+      </section>
+    </main>
   );
 }
 
